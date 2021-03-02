e2a732a50a62c648defba72a9c7e3f8abd5639f6
Dumebi <chiwuzohdumebi@gmail.com>
2021-03-02 18:05:58 +0100

    zipped files

--- routes/user.routes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const { authUser, registerUser } = require("../controllers/users.controller");
-
-const router = express.Router();
-
-router.route("/signup").post(registerUser);
-router.route("/signin").post(authUser);
-
-module.exports = router;
